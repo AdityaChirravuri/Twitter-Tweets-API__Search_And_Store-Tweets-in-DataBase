ff1d96d59288ffcee8945df529cbd9cf46d75a81
Aditya Srikar <adityasrikarchas17405crl@gmail.com>
2022-06-25 19:39:21 +0530

    initial commit

---
+++ models/keywords.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const keywordSchema = new mongoose.Schema({
+    keyword: String
+});
+
+const keywords = new mongoose.model('keywords', keywordSchema);
+
+async function storeKeywordInformation(kwd) {
+    const keyword = new keywords({
+        keyword: kwd
+    });
+    await keyword.save();
+}
+
+async function fetchAllKeywordInformation() {
+    const data = await keywords.find({});
+    return data;
+}
+
+async function deleteSingleKeyword(kwd) {
+    await keywords.deleteMany({keyword: kwd});
+}
+
+async function deleteAllKeywords() {
+    await keywords.deleteMany({});
+}
+
+module.exports = {storeKeywordInformation, fetchAllKeywordInformation, deleteAllKeywords};
